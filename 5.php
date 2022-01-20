9220d050fb614d61af09bd55fd60e8a8b8b3a240
kolan <kolanikk1@gmail.com>
2022-01-20 12:55:35 +0200

    resolved task 1-5

--- 5.php
+++ 5.php
@@ -1,6 +1,25 @@
 <?php
-$a =array(5,9,0,4,2,7,1);
-function min(){
-
+$a = [5, 9, 3, 4, 2, 7, 15, 6, 22];
+function myMin($myArray){
+$min = $myArray[0];
+for ($i=0;$i<count($myArray);$i++){
+    if ($min>$myArray[$i]){
+        $min=$myArray[$i];
+    }
+    }
+    echo $min;
 }
+function myMax($myArray){
+    $max = $myArray[0];
+    for ($i=0;$i<count($myArray);$i++){
+        if ($max<$myArray[$i]){
+            $max=$myArray[$i];
+        }
+    }
+    echo $max;
+}
+echo myMin($a) . " Минимальное значение";
+echo "<br>";
+echo myMax($a) . " Максимальное значение"
+
 ?>
